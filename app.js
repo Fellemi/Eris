6e91104a85340d9b96ce9828fe4cdf9c35c32c2e
Fellemi <felicia.karppi.satero@gmail.com>
2022-05-15 14:13:19 +0200

    did a lot, mostly on the planet sub-page

---
+++ app.js
@@ -0,0 +1,18 @@
+//https://stackoverflow.com/questions/20147442/using-onclick-on-images-to-display-text
+
+function changeText(value) {
+    var div = document.getElementById("div");
+    var text = "";
+
+    if (value == 1) text += "Mercury is a planet";
+    if (value == 2) text += "In terms of something, Venus is better :P";
+    if (value == 3) text += "Earth is the only planet we have found life on.";
+    if (value == 4) text += "Mars has dust storms that can reach up to x km/h";
+    if (value == 5) text += "Jupiter is our soloar system's largest planet, and has a mass of similar ämnen of a star.";
+    if (value == 6) text += "Saturn has the most discovered moons of all planets, with 82 as of May 2022";
+    if (value == 7) text += "Uranus";
+    if (value == 8) text += "Neptune";
+
+
+    div.innerHTML = text;
+}
