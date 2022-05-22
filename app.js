654daa4c70091f091af49f61a4f3a7a97cd99cab
Fellemi <felicia.karppi.satero@gmail.com>
2022-05-22 15:31:56 +0200

    after hours of felsökning, finally solved js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ function changeText(value) {
     if (value == 2) text += "In terms of something, Venus is better :P";
     if (value == 3) text += "Earth is the only planet we have found life on.";
     if (value == 4) text += "Mars has dust storms that can reach up to x km/h";
-    if (value == 5) text += "Jupiter is our solar system's largest planet, and has a mass of similar ämnen of a star.";
+    if (value == 5) text += "Jupiter is our solar system's largest planet, and has a mass of similar components of a star.";
     if (value == 6) text += "Saturn has the most discovered moons of all planets, with 82 as of May 2022";
     if (value == 7) text += "Uranus";
     if (value == 8) text += "Neptune";
